c974fe69fa9fa014c56d229e2fe383e7e1c40f65
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-04-24 10:00:16 +0100

    Improves teh testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,24 @@
 
 namespace Tests;
 
+use App\Jobs\CreateOgImageJob;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Bus;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Bus::fake([CreateOgImageJob::class]);
+        
+    }
 
     public function login(User $user = null): User
     {
